81a49c588a25f478dd7ff077d261e17482199117
Sharath Chandra Chinnarigari <csharathchandra3@gmail.com>
2024-04-21 23:06:47 +0530

    Completed Api , Profile page and fixed errors

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,19 +8,19 @@ export const metadata = {
 
 const RootLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>
-        <Provider>
-          <div className="main">
-            <div className="gradient" />
-          </div>
-          <main className="app">
-            <Nav />
-            {children}
-          </main>
-        </Provider>
-      </body>
-    </html>
+      <html lang="en">
+        <body>
+          <Provider>
+            <div className="main">
+              <div className="gradient" />
+            </div>
+            <main className="app">
+              <Nav />
+              {children}
+            </main>
+          </Provider>
+        </body>
+      </html>
   );
 };
 
